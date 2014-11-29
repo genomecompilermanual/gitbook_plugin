329ce9f908d1948134b11c562620492292fda0a0
Ben Lewis <lewishnl@gmail.com>
2014-11-29 23:37:47 +0000

    Add contents code.

--- book/script.js
+++ book/script.js
@@ -1,6 +1,23 @@
 $( document ).ajaxSuccess(function() {
+  /* Disable past click actions */
   $('.summary>.chapter>.articles>.chapter>a').off('click');
+  /* Hide all not in use subsubsections */
   $('.summary>.chapter>.articles>.chapter>.articles').hide();
+  /* Show active subsubsections */
   $('.articles>.active').parent().show();
   $('.articles>.active').children('.articles').show();
+  /* Show subsections in page */
+  if ($('.chapter.active').size() > 0 && $('.book-body .articles').size() == 0)
+  {
+    $('.book-body .page-wrapper .page-inner').prepend('<br>');
+    $('.active>.articles').clone().prependTo('.book-body .page-wrapper .page-inner');
+    $('.book-body .page-wrapper .page-inner').prepend('<h3>Sections</h3>');
+  }
+  /* Show subsubsections in page */
+  if ($('.articles>.active>.articles').size() > 0 && $('.book-body .articles').size() == 0)
+  {
+    $('.book-body .page-wrapper .page-inner').prepend('<br>');
+    $('.articles>.active>.articles').clone().prependTo('.book-body .page-wrapper .page-inner');
+    $('.book-body .page-wrapper .page-inner').prepend('<h3>Subsections</h3>');
+  }
 });
